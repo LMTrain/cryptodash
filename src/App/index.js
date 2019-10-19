463d12ebccc53dd225f9c28d72c0db7f1aa9b7f6
LM <Prog@Laycons-iMac.domain>
2019-10-18 22:54:12 -0500

    Practicing with Extended CSS components

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
+import styled, {css} from 'styled-components';
+
+
+const MyButton = styled.div`
+  color: green;
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
+
 // function Welcome(props) {
 //   return <h1>Welcome to {props.name}</h1>;
 // }
@@ -9,7 +23,12 @@ class App extends Component {
   render() {
     return (
       // <Welcome name={"CryptoDash"}/>
-      <WelcomeMessage/>
+      <div>
+        <WelcomeMessage/>
+        <MyButton>Hello</MyButton>
+        <MyButton primary>Hello</MyButton>
+        <TomatoButton primary>Hello</TomatoButton>
+      </div>
     
     );
   }
