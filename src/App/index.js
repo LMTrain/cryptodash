ba0c18d4da2d336753da18a240600b56ed04c26e
LM <Prog@Laycons-iMac.domain>
2019-10-18 23:11:22 -0500

    Add Googlr Fonts

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
-import styled, {css} from 'styled-components';
+// import styled, {css} from 'styled-components';
 
 
-const MyButton = styled.div`
-  color: green;
-  ${props => props.primary && css`
-    color: palevioletred;
-  `}
-`
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
+// const MyButton = styled.div`
+//   color: green;
+//   ${props => props.primary && css`
+//     color: palevioletred;
+//   `}
+// `
+// const TomatoButton = styled(MyButton)`
+//   color: tomato;
+//   border-color: tomato;
+// `;
 
 // function Welcome(props) {
 //   return <h1>Welcome to {props.name}</h1>;
@@ -25,9 +25,9 @@ class App extends Component {
       // <Welcome name={"CryptoDash"}/>
       <div>
         <WelcomeMessage/>
-        <MyButton>Hello</MyButton>
+        {/* <MyButton>Hello</MyButton>
         <MyButton primary>Hello</MyButton>
-        <TomatoButton primary>Hello</TomatoButton>
+        <TomatoButton primary>Hello</TomatoButton> */}
       </div>
     
     );
