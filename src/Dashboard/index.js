1e23693b2d56b213542a1117d4cd6aaf45970696
LM Train <layconmm@msn.com>
2019-10-30 19:46:11 -0500

    Add CoinSpotlight

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,9 +1,23 @@
 import React from'react';
 import Page from '../Shared/Page';
+import styled from 'styled-components';
 import PriceGrid from './PriceGrid';
+import CoinSpotlight from './CoinSpotlight';
+
+const ChartGrid = styled.div`
+    display: grid;
+    margin-top: 20px;
+    grid-gap: 15px;
+    grid-template-columns: 1fr 3fr;
+`
+
 
 export default function(){
     return <Page name="dashboard">
         <PriceGrid/>
+        <ChartGrid>
+            <CoinSpotlight/>
+            <div> Chart goes here </div>
+        </ChartGrid>
     </Page>
 }
